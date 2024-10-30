3912247cef8843f0736d0aea114a9afba74d05fb
Zahrah Rashid <elevennobodies@gmail.com>
2024-10-30 19:08:23 -0400

    merging landing, login, reg pages

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Mail, Lock, Loader } from "lucide-react";
-import { Link } from "react-router-dom";
 import Input from "../components/Input";
+import { User, Mail, Lock } from "lucide-react";
+import { Link } from "react-router-dom";
+import PasswordStrengthMeter from "../components/PasswordStrength";
 
-const LoginPage = () => {
+const SignUpPage = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const isLoading = false;
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log("Name:", name);
   };
-
-  function doLogin() {
-    // send request with specified headers
-    // read response
-  }
 
   return (
     <div
@@ -28,14 +25,20 @@ const LoginPage = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
       >
         <div className="p-8">
           <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
-            Welcome Back
+            Create Account
           </h2>
-
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleSignUp}>
+            <Input
+              Icon={User}
+              type="text"
+              placeholder="Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
             <Input
               Icon={Mail}
               type="email"
@@ -50,36 +53,26 @@ const LoginPage = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <div className="flex items-center mb-6">
-              <Link
-                to="/forgot-password"
-                className="text-sm text-green-400 hover:underline"
-              >
-                Forgot Password?
-              </Link>
-            </div>
+            <PasswordStrengthMeter password={password} />
             <motion.button
+              type="submit"
+              className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white
+              font-bold rounded-lg shadow-lg hover:from-green-600
+              hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+              focus:ring-offset-gray-900 transition duration-200"
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.98 }}
-              className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold
-              rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2
-              focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
-              type="submit"
-              disabled={isLoading}
             >
-              {isLoading ? (
-                <Loader className="w-6 h-6 animate-spin mx-auto" />
-              ) : (
-                "Login"
-              )}
+              Sign Up
             </motion.button>
           </form>
         </div>
         <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
           <p className="text-sm text-gray-400">
-            Don't have an account?{" "}
-            <Link to={"/signup"} className="text-green-400 hover:underline">
-              Signup
+            Already have an account?{" "}
+            <Link to={"/login"} className="text-green-400 hover:underline">
+              {" "}
+              Login
             </Link>
           </p>
         </div>
@@ -88,4 +81,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignUpPage;
