88028601fed26e5106acddc6273ef78c35e85c11
MalakElsayed09 <156450834+MalakElsayed09@users.noreply.github.com>
2024-10-30 22:27:13 -0400

    Updated style LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -31,7 +31,7 @@ const LoginPage = () => {
         className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
       >
         <div className="p-8">
-          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-yellow-300 to-yellow-700 text-transparent bg-clip-text">
             Welcome Back
           </h2>
 
@@ -61,9 +61,9 @@ const LoginPage = () => {
             <motion.button
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.98 }}
-              className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold
-              rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2
-              focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+              className="w-full py-3 px-4 bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-700 text-white font-bold
+              rounded-lg shadow-lg hover:from-yellow-500 hover:to-yellow-500 focus:outline-none focus:ring-2
+              focus:ring-yellow-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
               type="submit"
               disabled={isLoading}
             >
@@ -76,9 +76,9 @@ const LoginPage = () => {
           </form>
         </div>
         <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-100">
             Don't have an account?{" "}
-            <Link to={"/signup"} className="text-green-400 hover:underline">
+            <Link to={"/signup"} className="text-yellow-400 hover:underline">
               Signup
             </Link>
           </p>
