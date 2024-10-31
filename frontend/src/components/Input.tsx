adbf988a2907de78549fd75f88756d3e26121d84
MalakElsayed09 <156450834+MalakElsayed09@users.noreply.github.com>
2024-10-30 22:19:10 -0400

    Update Input.tsx
    updated style

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -13,7 +13,7 @@ const Input: React.FC<InputProps> = ({ Icon, ...props }) => {
       <input
         {...props}
         className="w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700
-        focus:border-green-500 focus:ring-2 focus:ring-green-500 text-white placeholder-gray-400 transition
+        focus:border-yellow-500 focus:ring-2 focus:ring-yellow-500 text-white placeholder-gray-400 transition
         duration-200"
       />
     </div>
