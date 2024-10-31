f9fc1d231016d32c85bde89c06078707cb9a717e
MalakElsayed09 <156450834+MalakElsayed09@users.noreply.github.com>
2024-10-31 10:29:13 -0400

    Updated style Input.tsx

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -8,12 +8,12 @@ const Input: React.FC<InputProps> = ({ Icon, ...props }) => {
   return (
     <div className="relative mb-6">
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <Icon className="size-5 text-green-500" />
+        <Icon className="size-5 text-yellow-500" />
       </div>
       <input
         {...props}
         className="w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700
-        focus:border-yellow-500 focus:ring-2 focus:ring-yellow-500 text-white placeholder-gray-400 transition
+        focus:border-yellow-500 focus:ring-2 focus:ring-yellow-500 text-white placeholder-gray-300 transition
         duration-200"
       />
     </div>
