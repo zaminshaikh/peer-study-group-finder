95c5dbf50a41b9120ed8326e0220c177e43cce69
MalakElsayed09 <156450834+MalakElsayed09@users.noreply.github.com>
2024-10-30 22:35:42 -0400

    Updated style SignUpPage.tsx

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -18,7 +18,7 @@ const SignUpPage = () => {
   return (
     <div
       className="min-h-screen bg-gradient-to-br 
-      from-gray-900 via-green-900 to-emerald-900 flex items-center
+      from-gray-900 via-yellow-900 to-yellow-600 flex items-center
       justify-center relative overflow-hidden"
     >
       <motion.div
@@ -28,7 +28,7 @@ const SignUpPage = () => {
         className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
       >
         <div className="p-8">
-          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-700 text-transparent bg-clip-text">
             Create Account
           </h2>
           <form onSubmit={handleSignUp}>
@@ -56,9 +56,9 @@ const SignUpPage = () => {
             <PasswordStrengthMeter password={password} />
             <motion.button
               type="submit"
-              className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white
-              font-bold rounded-lg shadow-lg hover:from-green-600
-              hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+              className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-700 text-white
+              font-bold rounded-lg shadow-lg hover:from-yellow-900
+              hover:to-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2
               focus:ring-offset-gray-900 transition duration-200"
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.98 }}
@@ -68,9 +68,9 @@ const SignUpPage = () => {
           </form>
         </div>
         <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-100">
             Already have an account?{" "}
-            <Link to={"/login"} className="text-green-400 hover:underline">
+            <Link to={"/login"} className="text-yellow-400 hover:underline">
               {" "}
               Login
             </Link>
