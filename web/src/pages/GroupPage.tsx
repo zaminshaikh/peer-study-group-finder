e683a24d9d604835b207b6abaca0a294a8090c83
Zahrah Rashid <elevennobodies@gmail.com>
2024-11-06 14:28:26 -0500

    placeholders for sidebar components

---
+++ web/src/pages/GroupPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GroupsPage: React.FC = () => {
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <h1 className="text-3xl font-bold text-white">My Groups</h1>
+    </div>
+  );
+};
+
+export default GroupsPage;
---
+++ web/src/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <h1 className="text-3xl font-bold text-white">My Profile</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
