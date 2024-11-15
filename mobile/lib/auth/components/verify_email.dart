ff99dfbf69c5606e87d976ced74210bcf5beb704
Zamin Shaikh <zaminusa@gmail.com>
2024-11-15 18:02:29 -0500

    Add user model import to verify_email.dart for improved user handling

--- mobile/lib/auth/components/verify_email.dart
+++ mobile/lib/auth/components/verify_email.dart
@@ -1,6 +1,7 @@
 // lib/pages/verify_email.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile/models/user_model.dart';
 import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
